c5d13b090d3fc5e6de6ef913e453f43266b0fd3a
nabixka <nabixka05@gmail.com>
2025-09-09 07:06:50 +0700

    Add Connect.php/database

---
+++ config/connect.php
@@ -0,0 +1,13 @@
+<?php
+    class connect{
+        private $host = "localhost";
+        private $user = "root";
+        private $pass = "bixkafa";
+        private $db = "daftarpahlawan";
+
+        public function con(){
+            $con = mysqli_connect("$this->host, $this->user, $this->pass, $this->db");
+            return $con;
+        }
+    }
+?>
